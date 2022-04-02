21dc7aa2d2dfd2872aaff76f0b61c4d0549d6e5f
chuckkahn <chuck.kahn@post-in-toronto.on.ca>
2022-04-02 01:49:43 -0400

    split path variables

--- exif_change.pl
+++ exif_change.pl
@@ -6,9 +6,15 @@ use File::Find::Rule;	# find all the subdirectories of a given directory
 
 use Image::ExifTool qw(:Public);
 
-my $path_DCIM = "/Volumes/CK_10TB/Downloads iMac/_media for unRAID/Insta360 ONE X/2022-03-18 - Toronto Comicon Day 1/DCIM/";
+my $Insta360_main   = "/Volumes/CK_10TB/Downloads iMac/_media for unRAID/Insta360 ONE X/";  # main path
 
-my $path_screenshot="/Volumes/CK_10TB/Downloads iMac/_media for unRAID/Insta360 ONE X/2022-03-18 - Toronto Comicon Day 1/_Studio/";
+my $Insta360_date   = "2022-03-20 - Toronto Comicon Day 3";
+
+# my $Insta360_date   = "2022-03-20";   # date part of path
+
+my $path_DCIM       = $Insta360_main . $Insta360_date . "/DCIM/";   # DCIM path
+
+my $path_screenshot = $Insta360_main . $Insta360_date . "/_Studio/";    # jpeg screenshot path
 
 # go through DCIM folders to gather locations of .insp files
 
@@ -40,10 +46,10 @@ my $c = 0;
 print "\n=================== go through screenshots ";
 
 my @folders_screenshot = File::Find::Rule->file
-                              ->name( 'IMG_20220318_094121_*_screenshot.jpg' )
+                              ->name( '*.jpg' )
 							  ->in( $path_screenshot );		# looking for .insp files
 
-print "under $path_screenshot\n\n";
+print "under $path_screenshot =========================================================\\n\n";
 
 my $exifTool = new Image::ExifTool;         # define the $exifTool variable
 
@@ -68,9 +74,19 @@ foreach my $screenshot (sort @folders_screenshot)				# going through the directo
     ++$c;                                               # increment counter
 
     # 2022-03-25_09-38-07_screenshot --- remove the last 35 characters of the screenshot filename, the remainder of which references the original .insp file
-    
-    my $insp_part = substr $ref_screenshot, 0, -35;
-    my $insp_ref = $insp_part . ".insp";                   # recreate .insp filename for reference
+
+    my $insp_ref;
+
+    if ( $ref_screenshot =~ /_screenshot.jpg/ )
+    {
+        my $insp_part = substr $ref_screenshot, 0, -35;
+        $insp_ref = $insp_part . ".insp";                   # recreate .insp filename for reference
+    }
+    else
+    {
+        $insp_ref = $ref_screenshot;
+        $insp_ref =~ s/.jpg/.insp/;
+    }
 
     print "=========================== screenshot #$c from .insp to .jpeg =========================================================\n\n";
 
@@ -87,11 +103,21 @@ foreach my $screenshot (sort @folders_screenshot)				# going through the directo
 
     my $info_DCIM = ImageInfo($insp_path{$insp_ref});
 
+
     printf $formattted, $$info_DCIM{DateTimeOriginal}, $$info_DCIM{Model}, $insp_ref;
+
+    # If you want to copy a time with SetNewValuesFromFile, then write a shifted value, you could set the GlobalTimeShift API option before calling SetNewValuesFromFile.  Of course, this would then apply to all date/time tags copied
+
+    # Time shift to apply to all extracted date/time PrintConv values. Does not affect ValueConv values
+
+    # Date/time shift string with leading '-' for negative shifts
+
+    my $info = $exifTool->Options(GlobalTimeShift => '-4');
 
     my $info = $exifTool->SetNewValuesFromFile($insp_path{$insp_ref}, 'DateTimeOriginal', 'Model');    # attempting to use the .insp file as the source for DateTimeOriginal
 
-    $exifTool->SetNewValue(DateTimeOriginal => '4:00', Shift => -1);    # shift time by 4 hours for GMT to EST
+
+    # $exifTool->SetNewValue(DateTimeOriginal => '4:00', Shift => -1);    # shift time by 4 hours for GMT to EST
 
     # write EXIF to screenshot jpeg
 
@@ -103,8 +129,5 @@ foreach my $screenshot (sort @folders_screenshot)				# going through the directo
     print "\n";
     
     $exifTool->SetNewValue();
-
-    # updated April 1, 2022, 12:30pm
-exit;
 }
 
